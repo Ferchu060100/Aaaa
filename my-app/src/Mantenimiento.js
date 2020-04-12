2cf450c33f8231cd22d7a7fc3d6d9d4e66ee40ba
Martin Ng <u201520759@upc.edu.pe>
2020-04-12 18:21:05 -0500

    mantenimiento

--- my-app/src/Mantenimiento.js
+++ my-app/src/Mantenimiento.js
@@ -1,24 +1,63 @@
 import React from 'react';
-import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
+import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Slider from '@material-ui/core/Slider';
+import Switch from '@material-ui/core/Switch';
+import FormGroup from '@material-ui/core/FormGroup';
 const useStyles = makeStyles({
   root: {
     height: 300,
   },
 });
-
+const AntSwitch = withStyles((theme) => ({
+  root: {
+    width: 28,
+    height: 16,
+    padding: 0,
+    display: 'flex',
+  },
+  switchBase: {
+    padding: 2,
+    color: theme.palette.grey[500],
+    '&$checked': {
+      transform: 'translateX(12px)',
+      color: theme.palette.common.white,
+      '& + $track': {
+        opacity: 1,
+        backgroundColor: theme.palette.primary.main,
+        borderColor: theme.palette.primary.main,
+      },
+    },
+  },
+  thumb: {
+    width: 12,
+    height: 12,
+    boxShadow: 'none',
+  },
+  track: {
+    border: `1px solid ${theme.palette.grey[500]}`,
+    borderRadius: 16 / 2,
+    opacity: 1,
+    backgroundColor: theme.palette.common.white,
+  },
+  checked: {},
+}))(Switch);
 function valuetext(value) {
   return `${value}°C`;
 }
 export default function Mantenimiento() {
   const classes = useStyles();
-
+  const [state, setState] = React.useState({
+    checkedA: true,
+    checkedB: true,
+    checkedC: true,
+  });
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
   return (
     <Container component="main" maxWidth="lg">
       <Grid
@@ -28,44 +67,205 @@ export default function Mantenimiento() {
   alignItems="center"
 >
 
-<Grid item xs={4} sm={2}>
+<Grid container direction="column" xs={4} sm={2} spacing={4}>
 <Typography component="h1">
           Temperatura
         </Typography>
-        <div className={classes.root}>
+        <div class="row" style={{justifyContent: 'space-around'}}>
+        <div class="column" className={classes.root}>
+        <Typography variant="subtitle2">
+          Mínima
+        </Typography>
         <Slider
           orientation="vertical"
           getAriaValueText={valuetext}
           defaultValue={30}
           aria-labelledby="vertical-slider"
         />
-                <Slider
+        </div>
+        <div class="column" className={classes.root}>
+        <Typography variant="subtitle2">
+          Máxima
+        </Typography>
+        <Slider
           orientation="vertical"
           getAriaValueText={valuetext}
           defaultValue={30}
           aria-labelledby="vertical-slider"
         />
-      </div>
+        </div>
+        </div>
+        <div class="row" style={{justifyContent: 'center'}}>
+        <Typography component="div">
+        <Grid component="label" container alignItems="center" spacing={1}>
+          <Grid item>Off</Grid>
+          <Grid item>
+            <AntSwitch checked={state.checkedC} onChange={handleChange} name="checkedC" />
+          </Grid>
+          <Grid item>On</Grid>
+        </Grid>
+      </Typography>
+        </div>
 </Grid>
   <Grid item xs={4} sm={2}>
 <Typography component="h1">
           Humedad
         </Typography>
+        <div class="row" style={{justifyContent: 'space-around'}}>
+        <div class="column" className={classes.root}>
+        <Typography variant="subtitle2">
+          Mínima
+        </Typography>
+        <Slider
+          orientation="vertical"
+          getAriaValueText={valuetext}
+          defaultValue={30}
+          aria-labelledby="vertical-slider"
+        />
+        </div>
+        <div class="column" className={classes.root}>
+        <Typography variant="subtitle2">
+          Máxima
+        </Typography>
+        <Slider
+          orientation="vertical"
+          getAriaValueText={valuetext}
+          defaultValue={30}
+          aria-labelledby="vertical-slider"
+        />
+        </div>
+        </div>
+        <div class="row" style={{justifyContent: 'center'}}>
+        <Typography component="div">
+        <Grid component="label" container alignItems="center" spacing={1}>
+          <Grid item>Off</Grid>
+          <Grid item>
+            <AntSwitch checked={state.checkedC} onChange={handleChange} name="checkedC" />
+          </Grid>
+          <Grid item>On</Grid>
+        </Grid>
+      </Typography>
+        </div>
 </Grid>
 <Grid item xs={4} sm={2}>
 <Typography component="h1">
           Nivel de Ph
         </Typography>
+        <div class="row" style={{justifyContent: 'space-around'}}>
+        <div class="column" className={classes.root}>
+        <Typography variant="subtitle2">
+          Mínima
+        </Typography>
+        <Slider
+          orientation="vertical"
+          getAriaValueText={valuetext}
+          defaultValue={30}
+          aria-labelledby="vertical-slider"
+        />
+        </div>
+        <div class="column" className={classes.root}>
+        <Typography variant="subtitle2">
+          Máxima
+        </Typography>
+        <Slider
+          orientation="vertical"
+          getAriaValueText={valuetext}
+          defaultValue={30}
+          aria-labelledby="vertical-slider"
+        />
+        </div>
+        </div>
+        <div class="row" style={{justifyContent: 'center'}}>
+        <Typography component="div">
+        <Grid component="label" container alignItems="center" spacing={1}>
+          <Grid item>Off</Grid>
+          <Grid item>
+            <AntSwitch checked={state.checkedC} onChange={handleChange} name="checkedC" />
+          </Grid>
+          <Grid item>On</Grid>
+        </Grid>
+      </Typography>
+        </div>
 </Grid>
 <Grid item xs={4} sm={2}>
 <Typography component="h1">
           EC
         </Typography>
+        <div class="row" style={{justifyContent: 'space-around'}}>
+        <div class="column" className={classes.root}>
+        <Typography variant="subtitle2">
+          Mínima
+        </Typography>
+        <Slider
+          orientation="vertical"
+          getAriaValueText={valuetext}
+          defaultValue={30}
+          aria-labelledby="vertical-slider"
+        />
+        </div>
+        <div class="column" className={classes.root}>
+        <Typography variant="subtitle2">
+          Máxima
+        </Typography>
+        <Slider
+          orientation="vertical"
+          getAriaValueText={valuetext}
+          defaultValue={30}
+          aria-labelledby="vertical-slider"
+        />
+        </div>
+        </div>
+        <div class="row" style={{justifyContent: 'center'}}>
+        <Typography component="div">
+        <Grid component="label" container alignItems="center" spacing={1}>
+          <Grid item>Off</Grid>
+          <Grid item>
+            <AntSwitch checked={state.checkedC} onChange={handleChange} name="checkedC" />
+          </Grid>
+          <Grid item>On</Grid>
+        </Grid>
+      </Typography>
+        </div>
 </Grid>
 <Grid item xs={4} sm={2}>
 <Typography component="h1">
           Luminosidad
         </Typography> 
+        <div class="row" style={{justifyContent: 'space-around'}}>
+        <div class="column" className={classes.root}>
+        <Typography variant="subtitle2">
+          Mínima
+        </Typography>
+        <Slider
+          orientation="vertical"
+          getAriaValueText={valuetext}
+          defaultValue={30}
+          aria-labelledby="vertical-slider"
+        />
+        </div>
+        <div class="column" className={classes.root}>
+        <Typography variant="subtitle2">
+          Máxima
+        </Typography>
+        <Slider
+          orientation="vertical"
+          getAriaValueText={valuetext}
+          defaultValue={30}
+          aria-labelledby="vertical-slider"
+        />
+        </div>
+        </div>
+        <div class="row" style={{justifyContent: 'center'}}>
+        <Typography component="div">
+        <Grid component="label" container alignItems="center" spacing={1}>
+          <Grid item>Off</Grid>
+          <Grid item>
+            <AntSwitch checked={state.checkedC} onChange={handleChange} name="checkedC" />
+          </Grid>
+          <Grid item>On</Grid>
+        </Grid>
+      </Typography>
+        </div>
 </Grid>
 </Grid>
     </Container>
